3ae09883c5b113f87cc73fb5b5da267f423bb66a
Edison Moreland <edison@devdude.net>
2024-11-01 22:33:38 -0700

    Play with liberty file

--- synthesize.sh
+++ synthesize.sh
@@ -6,6 +6,7 @@ MODULES_DIR="${PROJECT_DIR}/modules"
 TECHMAPS_DIR="${PROJECT_DIR}/techmaps"
 
 NETLIST_SKIN="${PROJECT_DIR}/skin.svg"
+LW_LIBERTY_FILE="${PROJECT_DIR}/logicworld.lib"
 
 MODULE="$1"
 
@@ -14,15 +15,20 @@ MODULE="$1"
 # netlistsvg
 # sv2v v0.0.12
 
+cat "${LW_LIBERTY_FILE}"
+
 mkdir -p "${OUTPUT_DIR}"
 
 sv2v -w "${OUTPUT_DIR}" "${MODULES_DIR}/${MODULE}.sv"
 yosys -o "${OUTPUT_DIR}/${MODULE}.json" -S "${OUTPUT_DIR}/${MODULE}.v" \
     -p 'prep -auto-top -flatten' \
-    -p 'abc -g AND,XOR,OR; opt -full' \
+    -p 'abc -g OR,NOR,NAND,XOR,XNOR; opt -full' \
     -p 'freduce -inv; opt -full' \
     -p "techmap -autoproc -map ${TECHMAPS_DIR}/ff2latch.v; opt_merge" \
     -p 'dfflegalize -cell $_DLATCH_P_ 0' \
+    # -p 'abc -liberty logicworld.lib'\
+    # -p 'read_liberty -lib logicworld.lib'\
+    # -p "dfflibmap -liberty logicworld.lib"\
 
 # TODO: Elk layout file to increase readibility?
 netlistsvg "${OUTPUT_DIR}/${MODULE}.json" -o "${OUTPUT_DIR}/${MODULE}.svg" --skin "${NETLIST_SKIN}"
