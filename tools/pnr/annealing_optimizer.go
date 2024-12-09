cba9dbb642efb88cd9c9820d2f4ce3eab33e2f72
Edison Moreland <edison@devdude.net>
2024-12-09 10:51:54 -0800

    Performance improvements

--- tools/pnr/annealing_optimizer.go
+++ tools/pnr/annealing_optimizer.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
-	rl "github.com/gen2brain/raylib-go/raylib"
 	"math"
 	"math/rand/v2"
 	"strconv"
+	"time"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 const nodeSize = 10
 const nodeDistance = 60
 const aspectRatio = 0.5
 const maxSteps = 99_9999
+const progressUpdates = 100
 
 type annealingOptimizer struct {
 }
@@ -71,7 +74,7 @@ func (a annealingOptimizer) Optimize(graph *NodeGraph) [][]NodeIndex {
 
 func visualizeNodeGraph(graph *NodeGraph, middleLayers [][]NodeIndex) {
 
-	nodePositions := map[NodeIndex]rl.Vector2{}
+	nodePositions := make([]rl.Vector2, len(graph.Nodes))
 	placeNodes(graph, middleLayers, nodeDistance, aspectRatio, func(nodeId NodeIndex, position rl.Vector2) {
 
 		nodePosition := rl.Vector2Add(position, rl.NewVector2(nodeDistance/2, nodeDistance/2))
@@ -127,6 +130,8 @@ func anneal[T any](state T, energy func(T) float32, neighbour func(T, float32) T
 	eLow := float32(math.Inf(1))
 	eHigh := float32(math.Inf(-1))
 
+	startTime := time.Now()
+
 	s := state
 	for i := 0; i < maxSteps; i++ {
 		temp := temperature(1.0 - float32(i+1)/float32(maxSteps))
@@ -140,12 +145,15 @@ func anneal[T any](state T, energy func(T) float32, neighbour func(T, float32) T
 
 		prob := p(sEnergy, newSEnergy, temp)
 
-		fmt.Printf("T = %f, E(s) = %f, E(s') = %f, P(E(s), E(s')) = %f, eLow = %f, eHigh = %f\n", temp, sEnergy, newSEnergy, prob, eLow, eHigh)
+		if i%(maxSteps/progressUpdates) == 0 {
+			elapsed := time.Since(startTime)
+			fmt.Printf("%s: T = %f, E(s) = %f, E(s') = %f, P(E(s), E(s')) = %f, eLow = %f, eHigh = %f\n", elapsed.String(), temp, sEnergy, newSEnergy, prob, eLow, eHigh)
+		}
 
 		if prob >= rand.Float32() {
 			s = newS
 			visualizeStep(s)
-			fmt.Println("chose new state")
+			// fmt.Println("chose new state")
 		}
 	}
 
@@ -153,14 +161,13 @@ func anneal[T any](state T, energy func(T) float32, neighbour func(T, float32) T
 }
 
 func energy(graph *NodeGraph) func([][]NodeIndex) float32 {
+	nodePositions := make([]rl.Vector2, len(graph.Nodes))
 	return func(layers [][]NodeIndex) float32 {
-		nodePositions := map[NodeIndex]rl.Vector2{}
-
 		placeNodes(graph, layers, nodeDistance, aspectRatio, func(nodeId NodeIndex, position rl.Vector2) {
 			nodePositions[nodeId] = position
 		})
 
-		foreachConnection := func(layerA, layerB []NodeIndex, f func(nodeA, nodeB NodeIndex)) {
+		foreachForwardConnection := func(layerA []NodeIndex, f func(nodeA, nodeB NodeIndex)) {
 			for _, nodeA := range layerA {
 				for _, nodeB := range graph.Nodes[nodeA].ForwardConnections {
 					f(nodeA, nodeB)
@@ -172,8 +179,8 @@ func energy(graph *NodeGraph) func([][]NodeIndex) float32 {
 		highestLength := float32(0.0)
 		connections := 0
 
-		measureConnections := func(layerA, layerB []NodeIndex) {
-			foreachConnection(layerA, layerB, func(nodeA, nodeB NodeIndex) {
+		measureConnections := func(layerA []NodeIndex) {
+			foreachForwardConnection(layerA, func(nodeA, nodeB NodeIndex) {
 				length := rl.Vector2Distance(nodePositions[nodeA], nodePositions[nodeB])
 
 				totalLength += length
@@ -182,31 +189,27 @@ func energy(graph *NodeGraph) func([][]NodeIndex) float32 {
 			})
 		}
 
-		previousLayer := graph.InputNodes
+		measureConnections(graph.InputNodes)
 		for _, layer := range layers {
-			measureConnections(layer, previousLayer)
-			previousLayer = layer
+			measureConnections(layer)
 		}
-		measureConnections(previousLayer, graph.OutputNodes)
 		averageLength := totalLength / float32(connections)
 
 		//return (averageLength) / float32(len(graph.Nodes))
 
 		//Count intersections
-		countIntersections := func(layerA, layerB []NodeIndex) int {
+		countIntersections := func(layerA []NodeIndex) int {
 			intersections := 0
 
-			foreachConnection(layerA, layerB, func(nodeAA, nodeAB NodeIndex) {
-				foreachConnection(layerA, layerB, func(nodeBA, nodeBB NodeIndex) {
+			foreachForwardConnection(layerA, func(nodeAA, nodeAB NodeIndex) {
+				foreachForwardConnection(layerA, func(nodeBA, nodeBB NodeIndex) {
 					if nodeAA == nodeBA && nodeAB == nodeBB {
 						return
 					}
 
-					collisionPoint := rl.NewVector2(0, 0)
-					if rl.CheckCollisionLines(
+					if linesIntersect(
 						nodePositions[nodeAA], nodePositions[nodeAB],
 						nodePositions[nodeBA], nodePositions[nodeBB],
-						&collisionPoint,
 					) {
 						intersections += 1
 					}
@@ -217,12 +220,11 @@ func energy(graph *NodeGraph) func([][]NodeIndex) float32 {
 		}
 
 		intersections := 0
-		previousLayer = graph.InputNodes
+		intersections += countIntersections(graph.InputNodes)
 		for _, layer := range layers {
-			intersections += countIntersections(layer, previousLayer)
-			previousLayer = layer
+			intersections += countIntersections(layer)
 		}
-		intersections += countIntersections(previousLayer, graph.OutputNodes)
+
 		//
 		//return (float32(intersections) / float32(len(graph.Nodes)))
 		return (float32(intersections) / float32(len(graph.Nodes))) * (averageLength / float32(len(graph.Nodes)))
@@ -255,18 +257,18 @@ func neighbour(graph *NodeGraph) func(layers [][]NodeIndex, temp float32) [][]No
 			//	newLayers[i][firstNode], newLayers[i][secondNode] = newLayers[i][secondNode], newLayers[i][firstNode]
 			//}
 
-			swaps := int(float32(len(newLayers[i])) * temp)
+			//swaps := int(float32(len(newLayers[i])) * temp)
 
-			for j := 0; j < swaps; j++ {
-				firstNode := rand.IntN(len(newLayers[i]))
-				secondNode := firstNode
-				for firstNode == secondNode {
-					secondNode = rand.IntN(len(newLayers[i]))
-				}
-
-				newLayers[i][firstNode], newLayers[i][secondNode] = newLayers[i][secondNode], newLayers[i][firstNode]
-
-			}
+			//for j := 0; j < swaps; j++ {
+			//	firstNode := rand.IntN(len(newLayers[i]))
+			//	secondNode := firstNode
+			//	for firstNode == secondNode {
+			//		secondNode = rand.IntN(len(newLayers[i]))
+			//	}
+			//
+			//	newLayers[i][firstNode], newLayers[i][secondNode] = newLayers[i][secondNode], newLayers[i][firstNode]
+			//
+			//}
 		}
 
 		// Pick a random layer then swap two nodes
@@ -274,7 +276,7 @@ func neighbour(graph *NodeGraph) func(layers [][]NodeIndex, temp float32) [][]No
 		swaps := 1
 
 		for i := 0; i < swaps; i++ {
-			layer := rand.IntN(len(newLayers))
+			layer := newLayers[rand.IntN(len(newLayers))]
 
 			// shuffle layer
 			//rand.Shuffle(len(newLayers[layer]), func(k, l int) {
@@ -282,16 +284,37 @@ func neighbour(graph *NodeGraph) func(layers [][]NodeIndex, temp float32) [][]No
 			//})
 
 			// swap two nodes
-			firstNode := rand.IntN(len(newLayers[layer]))
-			secondNode := firstNode
-			for firstNode == secondNode {
-				secondNode = rand.IntN(len(newLayers[layer]))
-			}
-
-			newLayers[layer][firstNode], newLayers[layer][secondNode] = newLayers[layer][secondNode], newLayers[layer][firstNode]
+			firstNode, secondNode := pickNodePair(layer)
+			swapNodes(layer, firstNode, secondNode)
 		}
 
 		return newLayers
+	}
+}
+
+func pickNodePair(layer []NodeIndex) (int, int) {
+	a := rand.IntN(len(layer))
+	b := a
+	for a == b {
+		b = rand.IntN(len(layer))
+	}
+	return a, b
+}
+
+func swapNodes(layer []NodeIndex, a, b int) {
+	// Swap a and b and all nodes between
+	if a < b {
+		a, b = b, a
+	}
+
+	if rand.IntN(1) == 1 {
+		for a > b {
+			layer[a], layer[b] = layer[b], layer[a]
+			a -= 1
+			b += 1
+		}
+	} else {
+		layer[a], layer[b] = layer[b], layer[a]
 	}
 }
 
@@ -337,4 +360,31 @@ func placeNodes(graph *NodeGraph, layers [][]NodeIndex, nodeDistance, aspectRati
 	}
 
 	placeLayer(currentY, graph.OutputNodes)
+}
+
+func linesIntersect(p1, q1, p2, q2 rl.Vector2) bool {
+	// Helper function to calculate orientation
+	orientation := func(p, q, r rl.Vector2) int {
+		val := (q.Y-p.Y)*(r.X-q.X) - (q.X-p.X)*(r.Y-q.Y)
+		if val == 0 {
+			return 0 // Collinear
+		} else if val > 0 {
+			return 1 // Clockwise
+		} else {
+			return 2 // Counterclockwise
+		}
+	}
+
+	// Calculate orientations
+	o1 := orientation(p1, q1, p2)
+	o2 := orientation(p1, q1, q2)
+	o3 := orientation(p2, q2, p1)
+	o4 := orientation(p2, q2, q1)
+
+	// General case
+	if o1 != o2 && o3 != o4 {
+		return true
+	}
+
+	return false
 }
--- tools/pnr/go.mod
+++ tools/pnr/go.mod
@@ -5,10 +5,12 @@ go 1.22.0
 toolchain go1.22.2
 
 require (
-	github.com/deckarep/golang-set/v2 v2.7.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.7.0
+	github.com/gen2brain/raylib-go/raylib v0.0.0-20241207114308-a9ad86d5018c
+)
+
+require (
 	github.com/ebitengine/purego v0.8.1 // indirect
-	github.com/gen2brain/raylib-go/raylib v0.0.0-20241203091912-3b89a24e68d1 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
-	golang.org/x/exp v0.0.0-20241204233417-43b7b7cde48d // indirect
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 )
